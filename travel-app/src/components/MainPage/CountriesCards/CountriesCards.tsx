2f1e63b596f7340fae7c331c45a41dedfb9cf2f4
Sakharuk Viktar <natgeogr93@gmail.com>
2021-03-15 00:45:09 +0300

    feat: implement search feature

--- travel-app/src/components/MainPage/CountriesCards/CountriesCards.tsx
+++ travel-app/src/components/MainPage/CountriesCards/CountriesCards.tsx
@@ -4,30 +4,54 @@ import './CountriesCards.css'
 
 import data from '../../../countriesData/data'
 
-export default function CountriesCards() {
+interface CountriesCardsProps {
+  search: string;
+}
+
+export default function CountriesCards({search}: CountriesCardsProps) {
+
+  const isMainEmpty = [...data].every(({country, capital: { name }}) => {
+    return !(country.toLowerCase().includes(search.toLowerCase())
+      || name.toLowerCase().includes(search.toLowerCase()))
+  });
+ 
+  function getSelectSearchClass(val: string, isCountryFind: boolean): string {
+    const isSearchEmpty = val === '';
+    if (isSearchEmpty) {
+      return '';
+    }
+    return isCountryFind ? 'succes-search' : '';
+  }
 
   return (
     <div className='main'>
-      {data.map(countryObj => {
-        const { photoURL, country, capital: { name } } = countryObj;
+      { isMainEmpty ? <div>No data available for this search query...</div> : 
+        [...data].map(countryObj => {
+          const { photoURL, country, capital: { name } } = countryObj;
+          const isSearchSucces = [country, name].some( elem => elem.toLowerCase().includes(search.toLowerCase()));
+          const isCountryFind = country.toLowerCase().includes(search.toLowerCase());
 
-        return (
-            <Card
-              key={country}
-              className='country-card'
-              href='/'>
-              <Image src={photoURL} />
-              <Card.Content>
-                <Card.Description>
-                  <b>{country}</b>
-                </Card.Description>
-                <Card.Description>
-                  <em>{name}</em>
-                </Card.Description>
-              </Card.Content>
-            </Card>
-        )
-      })}
+          return (
+            <>
+            {isSearchSucces && <Card
+                key={country}
+                className={`country-card`}
+                href='/'
+                >
+                <Image src={photoURL} />
+                <Card.Content>
+                  <Card.Description>
+                    <b className={getSelectSearchClass(search, isCountryFind)}>{country}</b>
+                  </Card.Description>
+                  <Card.Description>
+                    <em className={getSelectSearchClass(search, !isCountryFind)}>{name}</em>
+                  </Card.Description>
+                </Card.Content>
+              </Card>}
+            </>
+          )
+        })
+      }
     </div>
   )
 }
