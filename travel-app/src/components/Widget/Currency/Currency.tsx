c9b381462bcbb856407201cf72d44472e667bdbe
Sakharuk Viktar <natgeogr93@gmail.com>
2021-03-16 16:21:26 +0300

    fix: fix currency display

--- travel-app/src/components/Widget/Currency/Currency.tsx
+++ travel-app/src/components/Widget/Currency/Currency.tsx
@@ -6,11 +6,23 @@ export default class Currency extends Component<{[key: string]: any}> {
     const { rates } = this.props;
     const currenciesArray: string[] = ['EUR', 'USD', 'BYN'];
 
+    if (!rates) return (<div>no currency data</div>)
+
+    const rateArray = currenciesArray.map( curr => {
+      const rate = rates[curr];
+
+      if (rate < 1) {
+        const group = 10 ** (`${Math.trunc(1/rate)}`.length - 1);
+        return `${group} : ${(rate*group).toFixed(3)} ${curr}`
+      }
+      return `1: ${rate.toFixed(3)} ${curr}`;
+    })
+
     return (
       <div className='widget-currency'>
         <Fragment>
-          {currenciesArray.map(curr => (
-            <div key={curr} className={`widget-currency__${curr}`}>1 : {rates && (rates[curr]).toFixed(3)} {curr}</div>
+          {rateArray.map((curr, index) => (
+            <div key={index} className={`widget-currency__${currenciesArray[index]}`}>{curr}</div>
           ))}
         </Fragment>
       </div>
