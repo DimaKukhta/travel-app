83392675421d788dc1ab35cfa7f6a6ade76dc9a0
Sakharuk Viktar <natgeogr93@gmail.com>
2021-03-17 23:31:45 +0300

    feat: translate rating

--- travel-app/src/components/country-page/RatingOfPlace/RatingOfPlace.tsx
+++ travel-app/src/components/country-page/RatingOfPlace/RatingOfPlace.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { Rating, Button } from 'semantic-ui-react';
+import translate from '../../../translateData/translate'
 import './ratingOfPlace.css';
 
 interface RatingOfPlaceProps {
     imgId: number,
-    isAuthorized: boolean
+    isAuthorized: boolean,
+    lang: string
 }
 
-export const RatingOfPlace:  React.FC<RatingOfPlaceProps> = ({imgId, isAuthorized}) => {
+export const RatingOfPlace:  React.FC<RatingOfPlaceProps> = ({imgId, isAuthorized, lang}) => {
     const [rating, setRating] = useState(0);
 
     const handleRate = (e: any, { rating }: any) => {
@@ -16,14 +18,14 @@ export const RatingOfPlace:  React.FC<RatingOfPlaceProps> = ({imgId, isAuthorize
 
     return (
         <div className="rating-of-place">
-            <h3>Rating</h3>
-            <h5>Average rating: {rating}</h5>
-            <Rating icon="star" maxRating={5} defaultRating={rating} onRate={handleRate}></Rating>
-            <div className="rating-button"><Button color="green" disabled={isAuthorized ? false : true}>Submit</Button></div>
+            <h3>{translate.rating.head[lang]}</h3>
+            <h5>{translate.rating.average[lang]}: {rating}</h5>
+            <Rating icon="star" maxRating={5} defaultRating={rating} onRate={handleRate} ></Rating>
+            <div className="rating-button"><Button color="green" disabled={isAuthorized ? false : true}>{translate.rating.submit[lang]}</Button></div>
            {/* <ul>
                 {currentValue.map((element: any, index: number) => <li key={index}>{element.login}, score: {element.rating}</li>)}
            </ul>*/}
-           {isAuthorized ? null : <div>To leave a rating please login.</div>}
+           {isAuthorized ? null : <div>{translate.rating.note[lang]}</div>}
         </div>
 
     )
